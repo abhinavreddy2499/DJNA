1bab4cc782275a7f3f31f62a6c5a61efc2eb91d9
Abhinav <abhinavreddy2499@gmail.com>
2023-04-28 13:58:11 -0400

    added docmentation for view component

--- src/view/view.py
+++ src/view/view.py
@@ -9,9 +9,10 @@ import json
 
 
 class MapView(Observer):
+    """Class for the map view"""
 
     def __init__(self):
-
+        """Initializing map view"""
         super().__init__()
 
         self.route_coordinates = None
@@ -19,9 +20,11 @@ class MapView(Observer):
         self.total_distance = None
 
     def get_route_params(self):
+        """Call to Retrieve route parameters"""
         return (self.route_coordinates, self.total_distance, self.elevation)
 
     def update(self, path_model):
+        """Call to Update the map view"""
         self.total_distance = path_model.get_length()
         self.route_coordinates = path_model.get_route()
 
