3395a5e0da6de995060e53deeecf8e84e65d4700
Jaswanth <jkommuru@umass.edu>
2023-04-29 11:28:31 -0400

    Added the fuction which calculates the cost of the route

--- src/model/methodology.py
+++ src/model/methodology.py
@@ -1,3 +1,9 @@
+INCREASED_ELEVATION = "elevation_gain"
+CAL_ELEVATION = "elevation"
+DISTANCE = "length"
+STANDARD = "normal"
+COST = "weight"
+
 class Methodology:
     """The initialization of crucial parameters, including the graph, method, path limit, and more, are handled by this class.
     It additionally contains methods for enlisting viewers, creating algorithm objects, outputting path data,
@@ -11,3 +17,12 @@ class Methodology:
         self.enable_value = 1
         self.algorithm = None
         self.increased_path = None
+
+    def get_cost_of_route(self, graph, path, cost_val):
+        """provides the route's cost back."""
+        aggregate = 0
+        itr = len(path) - 1
+        for a in range(itr):
+            w_cal = self.get_edge_cost(graph, path[a], path[a + 1], cost_val)
+            aggregate = aggregate + w_cal
+        return aggregate
