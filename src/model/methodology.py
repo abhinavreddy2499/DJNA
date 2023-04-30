74f39688f1cbd7139332faad37b91e4f66e7e600
Jaswanth <jkommuru@umass.edu>
2023-04-30 16:26:51 -0400

    Added getters and setters for the Methodology class

--- src/model/methodology.py
+++ src/model/methodology.py
@@ -40,3 +40,44 @@ class Methodology:
         elif type_imp == INCREASED_ELEVATION:
             cal_cost = graph.nodes[destination_point][CAL_ELEVATION] - graph.nodes[origin_point][CAL_ELEVATION]
             return max(0.0, cal_cost)
+        
+
+    def set_origin(self, origin):
+        """the origin attribute is set to the specified origin."""
+        self.origin = origin
+
+    def set_data_network(self, data_network):
+        """the provided data_network is set as the data_network attribute."""
+        self.data_network = data_network
+
+    def get_data_network(self):
+        """the data_network attribute is returned."""
+        return self.data_network
+
+    def set_chosen_elevation(self, chosen_elevation):
+        """the specified chosen_elevation value into the chosen_elevation attribute is set."""
+        self.chosen_elevation = chosen_elevation
+
+    def set_destination(self, destination):
+        """the end_point attribute is set to the specified end_point."""
+        self.end_point = destination
+
+    def set_enable_value(self, enable_value):
+        """provided enable_value to the attribute's value is set."""
+        self.enable_value = enable_value
+
+    def get_enable_value(self):
+        """the attribute enable_value is returned"""
+        return self.enable_value
+
+    def set_increased_path(self, increased_path):
+        """the specified increased_path is set as the increased_path attribute."""
+        self.increased_path = increased_path
+
+    def get_increased_path(self):
+        """gives the increased_path attribute back."""
+        return self.increased_path
+
+    def get_chosen_elevation(self):
+        """The chosen_elevation attribute is returned."""
+        return self.chosen_elevation
