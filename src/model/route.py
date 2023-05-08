8b7173b2692eb3fc5f30a15188b8f760414e121e
Jaswanth <jkommuru@umass.edu>
2023-05-08 09:01:35 -0400

    Added some getters and setters

--- src/model/route.py
+++ src/model/route.py
@@ -16,3 +16,21 @@ class Route(Viewing):
         self.decrease = 0
         self.increase = 0
         self.route = []
+
+    def get_start_point(self):
+        """starting point is obtained"""
+        return self.start_point
+
+    def set_end_point(self, end_point):
+        """ending point is set"""
+        self.end_point = end_point
+        self.new_value()
+
+    def get_end_point(self):
+        """end point is obtained"""
+        return self.end_point
+
+    def set_start_point(self, start_point):
+        """starting position is set"""
+        self.start_point = start_point
+        self.new_value()
