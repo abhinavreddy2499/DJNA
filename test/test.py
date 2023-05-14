486221a9678a79cc13f8cceb694f201640946f41
Neha <nehajhurani20@gmail.com>
2023-05-14 14:47:11 -0400

    added test case for shortest route path

--- test/test.py
+++ test/test.py
@@ -6,6 +6,7 @@ sys.path.insert(1, sys.path[0][:-5])
 import pickle as p
 import geopy
 import math
+from src.controller.ShortestRouteController import ShortestRouteController
 from src.model.network import Network
 import logging
 
@@ -33,6 +34,22 @@ def test_loading_network(end):
     assert isinstance(network, nx.classes.multidigraph.MultiDiGraph)
 
 
+@Test
+def test_get_shortest_route():
+    """
+    Testing get_shortest_route() of shortest route controller
+    :return:
+    """
+
+    start_point = (42.3732216, -72.5198537)
+    end_point = (42.4663727, -72.5795115)
+    network = Network().loading_network(end_point)
+    shortest_route_controller = ShortestRouteController(network)
+    shortest_path = shortest_route_controller.get_shortest_route(start_point, end_point)
+    assert abs(shortest_path.get_length() - 11956.737999999996) <= 100
+
+
 if __name__ == "__main__":
     start, end = (42.3732216, -72.5198537), (42.4663727, -72.5795115)
     test_loading_network(end)
+    test_get_shortest_route()
